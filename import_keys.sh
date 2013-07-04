ea41187b83a3b2d3dcc1aaba657c5ccbb2a9e49d
Aaron Sikes <jingadjanga@gmail.com>
2013-07-03 23:20:23 -0400

    Added a tweeter! index.js just posts a tweet with the current time. It looks at the environment for the api keys

---
+++ import_keys.sh
@@ -0,0 +1,13 @@
+echo Consumer key?
+read CONSUMER_KEY
+
+echo Consumer secret?
+read CONSUMER_SECRET
+
+echo Access token?
+read ACCESS_TOKEN
+
+echo Access secret?
+read ACCESS_SECRET
+
+export CONSUMER_KEY CONSUMER_SECRET ACCESS_TOKEN ACCESS_SECRET
